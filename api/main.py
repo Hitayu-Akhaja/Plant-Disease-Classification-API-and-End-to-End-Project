3042d51ad280cfaccb52a41c763abd682e250532
Hitayu-Akhaja <hitayuakhaja75@gmail.com>
2023-08-09 22:26:23 +0530

    Added Transfer Learning
    created another model using transfer learning

--- api/main.py
+++ api/main.py
@@ -22,9 +22,9 @@ app.add_middleware(
 
 
 
-MODEL = tf.keras.models.load_model("../saved_models/1")
+MODEL = tf.keras.models.load_model("../saved_models/2")
 
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+CLASS_NAMES = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot', 'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
 
 
 @app.get("/ping")
